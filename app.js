02b1360056e2ba7bf0c272de06f0f1d994cb8ed7
lukagolf <jovanovic.l@northeastern.edu>
2023-06-05 18:02:18 -0400

    A5: Finish up to Section 7

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+TuitsController(app);
+HelloController(app)
+UserController(app)
+app.listen(4000)
