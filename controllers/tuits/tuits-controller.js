630d3b53cea61abcc2b5c7ec688f350b60ad319d
lukagolf <jovanovic.l@northeastern.edu>
2023-06-06 15:45:22 -0400

    Fix the heart like and the content of th tuit on homepage

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,14 +1,44 @@
 import posts from "./tuits.js";
 let tuits = posts;
 
-const createTuit = (req, res) => {
+const createTuit = async (req, res) => {
     const newTuit = req.body;
+    newTuit.topic = "Space";
+    newTuit.username = "Nasa";
+    newTuit.handle = "@nasa";
+    newTuit.time = "0s";
+    newTuit.image = "nasa-logo.jpg";
+    newTuit.title = "Nasa's Mission";
     newTuit._id = (new Date()).getTime() + '';
     newTuit.likes = 0;
     newTuit.liked = false;
+    newTuit.replies = 0;
+    newTuit.retuits = 0;
     tuits.push(newTuit);
     res.json(newTuit);
 }
+
+/* const AddNewTuit = async (req,res) =>
+{
+    console.log("Inside controller")
+    let NewTuit = req.body;
+
+    NewTuit.likes = 0;
+    NewTuit.liked = false;
+
+    NewTuit.topic = "Traffic";
+    NewTuit.username = "The Boring Company";
+    NewTuit.handle = "@boringcompany";
+    NewTuit.time = "2h";
+    NewTuit.image = "../../Images/Tesla.png";
+    NewTuit.title = "The Boring Company fixing traffic";
+    NewTuit.replies = 0;
+    NewTuit.dislikes = 0;
+    NewTuit.retuits = 0;
+    console.log(NewTuit);
+    const InsertedTuit = await TuitsDAO.createTuit(NewTuit);
+    res.send(InsertedTuit );
+} */
 
 const findTuits = (req, res) => res.json(tuits);
 
