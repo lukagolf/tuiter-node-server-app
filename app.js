dca85e637680ffc7d53654817e33c9cf56b4a030
lukagolf <jovanovic.l@northeastern.edu>
2023-06-06 18:46:35 -0400

    A5: Finish up to 9

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ import express from 'express'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-import cors from 'cors'
+import cors from 'cors';
+import session from "express-session";
+import AuthController from "./users/auth-controller.js";
 
 const app = express()
-app.use(cors())
-app.use(express.json())
+app.use(
+    session({
+        secret: "any string",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:3000",
+    })
+);
+app.use(express.json());
+const port = process.env.PORT || 4000;
 TuitsController(app);
-HelloController(app)
-UserController(app)
-app.listen(4000)
+HelloController(app);
+UserController(app);
+AuthController(app);
+app.listen(process.env.PORT || 4000);
