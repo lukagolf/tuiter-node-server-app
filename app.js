c95bf01879877b6fe0700ba839501e0e8c28843b
lukagolf <jovanovic.l@northeastern.edu>
2023-06-15 18:10:56 -0400

    Ready for deployment, but with GLOBAL VARIABLE

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ import express from 'express'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-import cors from 'cors';
 import session from "express-session";
 import AuthController from "./users/auth-controller.js";
 import mongoose from "mongoose";
-import ConnectMongo from 'connect-mongo';
 import { config as dotenvConfig } from 'dotenv';
 
 dotenvConfig();
@@ -21,48 +19,13 @@ app.use(
         secret: "any string",
         resave: false,
         saveUninitialized: true,
-        // store: new MongoStore({
-        //     url: 'mongodb://127.0.0.1:27017/tuiter',
-        //     ttl: 14 * 24 * 60 * 60,
-        //     autoRemove: 'native'
-        // })
     })
 );
 
-// app.get('/', (req,res,next) => {
-//     req.session.user = {
-//         uuid: '12234-2345-2323423'
-//     }
-//     req.session.save(err => {
-//         if(err){
-//             console.log(err);
-//         } else {
-//             res.send(req.session.user)
-//         }
-//     });
-// })
-
-// app.get('/end', (req,res,next) => {
-//     req.session.destroy(err => {
-//         if(err){
-//             console.log(err);
-//         } else {
-//             res.send('Session is destroyed')
-//         }
-//     });
-// })
-
 app.use((req, res, next) => {
-    // const allowedOrigins = ["http://localhost:3000", "https://a6--stellular-malasada-11cc12.netlify.app"];
-    // const origin = req.headers.origin;
-
-    // if (allowedOrigins.includes(origin)) {
-    //     res.header("Access-Control-Allow-Origin", "*");
-    // }
     res.header("Access-Control-Allow-Headers", "Content-Type");
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH, OPTIONS");
-    // res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
 
