661c7c023e5135a818b7f35ac0a88276ca1b67d2
lukagolf <jovanovic.l@northeastern.edu>
2023-06-15 14:07:35 -0400

    Debug CORS block

--- app.js
+++ app.js
@@ -53,16 +53,16 @@ app.use(
 // })
 
 app.use((req, res, next) => {
-    const allowedOrigins = ["http://localhost:3000", "https://a6--stellular-malasada-11cc12.netlify.app"];
-    const origin = req.headers.origin;
+    // const allowedOrigins = ["http://localhost:3000", "https://a6--stellular-malasada-11cc12.netlify.app"];
+    // const origin = req.headers.origin;
 
-    if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
-    }
-
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // if (allowedOrigins.includes(origin)) {
+    //     res.header("Access-Control-Allow-Origin", "*");
+    // }
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH, OPTIONS");
-    res.header("Access-Control-Allow-Credentials", "true");
+    // res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
 
